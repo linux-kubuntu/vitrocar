a7e8f936c11ac7a0f3fbc4ef12f75bca2c8be523
Yeltsin <Yeltsin@192.168.121.191>
2018-08-18 11:31:29 -0500

    Added new files

---
+++ src/java/com/vitrocar/view/SelectOneView.java
@@ -0,0 +1,18 @@
+
+package com.vitrocar.view;
+
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class SelectOneView {
+     
+    private String option;
+     
+    public String getOption() {
+        return option;
+    }
+ 
+    public void setOption(String option) {
+        this.option = option;
+    }
+}
